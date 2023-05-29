4111ef1d2c0e7c43e0caa91e09dcb6570b4aa6d9
1818GEGE <131153205+1818GEGE@users.noreply.github.com>
2023-05-29 19:46:24 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'IMSYY 站点监测1',
+  SiteName: '18WEB 站点监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
@@ -18,16 +18,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '主页',
-      url: 'https://www.imsyy.top/'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.imsyy.top/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/imsyy/'
+      text: '十八导航',
+      url: 'https://dh.ufoxvpn.club/'
     },
   ],
 };
